7c098c891d18ca8be28a2a8594ae64007e406d02
umbraweb <159861347+umbraweb@users.noreply.github.com>
2024-03-26 18:30:40 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+word = input("Which word would you like to learn about today")
+keys = ('apple', 'ape')
+values =["A sweet, crisp, and refreshing fruit that typically has a red, green, or yellow skin.", 
+         "Anthropology, Zoology. any member of the superfamily Hominoidea, the two extant branches of which are the lesser apes (gibbons) and the great apes (humans, chimpanzees, gorillas, and orangutans)"]
+my_dictionary = (zip(keys, values))
+
