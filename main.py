0f59c7aba1926b895bab029e35f93733b1c70795
James <jamessgaskill@gmail.com>
2024-03-30 19:05:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-word = input("Which word would you like to learn about today")
-keys = ('apple', 'ape')
-values =["A sweet, crisp, and refreshing fruit that typically has a red, green, or yellow skin.", 
-         "Anthropology, Zoology. any member of the superfamily Hominoidea, the two extant branches of which are the lesser apes (gibbons) and the great apes (humans, chimpanzees, gorillas, and orangutans)"]
-my_dictionary = (zip(keys, values))
-
+word = input("Which word would you like to learn about today\n")
+keys = ('apple', 'ape', 'orange')
+values ={'apple' :"A sweet, crisp, and refreshing fruit that typically has a red, green, or yellow skin.", 'ape':  "Anthropology, Zoology. any member of the superfamily Hominoidea, the two extant branches of which are the lesser apes (gibbons) and the great apes (humans, chimpanzees, gorillas, and orangutans)"}
+if word in keys:
+  print(values[word])
+else:
+  print("Word not found")
